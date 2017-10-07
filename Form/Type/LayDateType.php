8499a39367a383bd327d76128085cff8b6c1c8e2
李春鹏 <lcp0578@gmail.com>
2017-10-08 01:05:31 +0800

    [MOD] update laydate form type and update useage

--- Form/Type/LayDateType.php
+++ Form/Type/LayDateType.php
@@ -5,15 +5,48 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormBuilderInterface;
 
-class LayDateType extends AbstractType 
+class LayDateType extends AbstractType
 {
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'width' => '100%',
-            'height' => '240px'
+            'placeholder' => ' - ',
+            'range' => 0,
+            'type' => ''
         ));
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     *
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        // For Symfony 2.0:
+        // $view->set('base_path', $form->getAttribute('base_path'));
+        
+        // For Symfony 2.1 and higher:
+        $view->vars['placeholder'] = $options['placeholder'];
+        $view->vars['range'] = $options['range'];
+        $view->vars['type'] = $options['type'];
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     *
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->setAttribute('placeholder', $options['placeholder'])
+        ->setAttribute('range', $options['range'])
+        ->setAttribute('type', $options['type']);
     }
     
     public function getParent()
--- README.md
+++ README.md
@@ -46,5 +46,33 @@ in the `app/AppKernel.php` file of your project:
 ### Step 3: Configuration 
 
 	# config.yml
+	# Twig Configuration
+	twig:
+	    ...
+	    form_themes:
+	        - 'KitFormBundle:Form:kit_form_theme.html.twig'
 
 ## Usage
+- LayDateType  
+	引入laydate.js 或者 使用 layui 框架。
+
+		<script src="{{ asset('public/laydate/laydate.js') }}"></script>
+
+	buildForm进行使用：
+
+		$builder-->add('start_time', LayDateType::class, [
+            'label' => '开始时间',
+            'type' => 'time'
+        ])->add('end_time', LayDateType::class, [
+            'label' => '结束时间',
+            'type' => 'time'
+        ]);
+
+	| type可选值	| 名称 |	用途 |
+	| :---: | :---:| :---: |
+	| year | 年选择器  |	只提供年列表选择 |
+	| month	| 年月选择器	| 只提供年、月选择 |
+	| date	| 日期选择器	| 可选择：年、月、日。type默认值，一般可不填 |
+	| time	| 时间选择器	| 只提供时、分、秒选择 |
+	| datetime	| 日期时间选择器	| 可选择：年、月、日、时、分、秒 |
+	PS: 详细配置请阅读[layDate官方文档](http://www.layui.com/laydate/)
