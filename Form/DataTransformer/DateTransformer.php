df5ba7ac6caa56eeb85a16949156a61a0baf1632
lcp0578 <lcp0578@gmail.com>
2019-05-29 11:07:23 +0800

    [MOD] add data transformer

---
+++ Form/DataTransformer/DateTransformer.php
@@ -0,0 +1,44 @@
+<?php
+namespace Kit\FormBundle\Form\DateTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class DateTransformer implements DataTransformerInterface
+{
+    /**
+     * Transforms an object (DateTime) to a string (date).
+     *
+     * @param  \DateTime|string $dateTime
+     * @return string
+     */
+    public function transform($dateTime)
+    {
+        if($dateTime instanceof \DateTime){
+            return $dateTime->format('Y-m-d H:i:s');
+        }
+        
+        return $dateTime;
+    }
+    
+    /**
+     * Transforms a string (date) to an object (DateTime).
+     *
+     * @param  string $dateStr
+     * @return string
+     * @throws TransformationFailedException if object (issue) is not found.
+     */
+    public function reverseTransform($dateStr)
+    {
+        try{
+            $dateTime = new \DateTime($dateStr);
+        }catch (\Exception $e) {
+            throw new TransformationFailedException(sprintf(
+                'DateTime convert error!',
+                $e->getMessage()
+                ));
+        }
+        
+        return $dateTime;
+    }
+}
